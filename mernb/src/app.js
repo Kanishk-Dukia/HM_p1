978742dde1a1a0f5feb21382b2f988b54d657d38
wut-semicolon <woekspace@gmail.com>
2023-01-15 00:12:11 +0530

    4

--- mernb/src/app.js
+++ mernb/src/app.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require("bcrypt");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
 require("./db/conn");
 const Register = require("./models/registers");
+const Query_report = require("./models/contacts");
 
 const port = process.env.PORT || 3000;
 
@@ -30,6 +32,14 @@ app.get("/register", (req, res) => {
   res.render("register");
 })
 
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+})
+
 
 // create a new user in out database
 app.post("/register", async (req, res) => {
@@ -43,16 +53,18 @@ app.post("/register", async (req, res) => {
       const registerEmployee = new Register({
         Full_name: req.body.Full_name,
         password: password,
-        confirm_password :cpassword,
-        Scholar_Number:req.body.Scholar_Number,
-        Phone_no:req.body.Phone_no,
-        Email:req.body.Email,
-        DOBs:req.body.DOBs,
-        Year_of_study:req.body.Year_of_study,
-        Course:req.body.Course,
-        Gender:req.body.Gender
-         
+        confirm_password: cpassword,
+        Scholar_Number: req.body.Scholar_Number,
+        Phone_no: req.body.Phone_no,
+        Email: req.body.Email,
+        DOBs: req.body.DOBs,
+        Year_of_study: req.body.Year_of_study,
+        Course: req.body.Course,
+        Gender: req.body.Gender
+
       })
+
+      // hash
 
       const registered = await registerEmployee.save();
       res.status(201).render("index");
@@ -66,6 +78,50 @@ app.post("/register", async (req, res) => {
     res.status(400).send(error);
   }
 })
+
+// login validation
+
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.Email;
+    const password = req.body.password;
+
+    const useremail = await Register.findOne({ Email: email });
+
+    const isMatch = await bcrypt.compare(password, useremail.password);
+
+    if (isMatch) {
+      res.status(201).render("index");
+    }
+    else {
+      res.send("invalid login details");
+    }
+  }
+  catch (error) {
+    res.status(400).send("invalid login details")
+  }
+})
+
+
+app.post("/contact", async (req, res) => {
+  try {
+
+    const Problem_form = new Query_report({
+      Name: req.body.Name,
+      Phone_n: req.body.Phone_n,
+      Email_id: req.body.Email_id,
+      Query_s: req.body.Query_s
+    })
+
+    const Problems = await Problem_form.save();
+    res.status(201).render("index");
+
+  } 
+  catch (error) {
+    res.status(400).send(error);
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`server is running at port no ${port} `);
--- mernb/src/models/registers.js
+++ mernb/src/models/registers.js
@@ -1,50 +1,62 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const employeeScheme = new mongoose.Schema({
-      Full_name :{
-        type:String,
+    Full_name: {
+        type: String,
         required: true
-      },
-      password :{
-        type:String,
+    },
+    password: {
+        type: String,
         required: true
-      },
-      confirm_password :{
-        type:String,
+    },
+    confirm_password: {
+        type: String,
         required: true
-      },
-      Scholar_Number:{
+    },
+    Scholar_Number: {
         type: Number,
         required: true,
         unique: true
-      },
-      Phone_no:{
+    },
+    Phone_no: {
         type: Number,
         required: true,
         unique: true
-      },
-      Email:{
-        type:String,
-        required:true,
-        unique:true
-      },
-      DOBs:{
-        type:String,
-        required:true
-      },
-      Year_of_study:{
-        type:Number,
-        required:true
-      },
-      Course:{
-        type:String,
-        required:true
-      },
-      Gender:{
-        type:String,
-        required:true
-      }
-      
+    },
+    Email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    DOBs: {
+        type: String,
+        required: true
+    },
+    Year_of_study: {
+        type: Number,
+        required: true
+    },
+    Course: {
+        type: String,
+        required: true
+    },
+    Gender: {
+        type: String,
+        required: true
+    }
+
+})
+
+employeeScheme.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+
+        this.confirm_password = undefined;
+    }
+
+    next();
+
 })
 
 
